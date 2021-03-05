d163344a4aad925d7b5f06371cbd7755c5baa139
sjkearney1228 <60799782+sjkearney1228@users.noreply.github.com>
2021-03-05 12:26:46 -0600

    Update runcode.py

--- runcode.py
+++ runcode.py
@@ -1,6 +1,11 @@
 #Necessary Python packages and dependencies
+import os
+import tensorflow.keras
+import h5py
 import numpy as np
-from numpy import load 
+from numpy import load
+import keras
+from keras.models import model_from_json
 import matplotlib.pyplot as plt
 
 def myspecto_new(data,window,nfft,shift,m):
@@ -19,6 +24,46 @@ def myspecto_new(data,window,nfft,shift,m):
 		out1[:,i] = tmp;
 	
 	return out1
+	
+def runmodel(data):
+
+	# load json and create model
+	#json_file = open('model.json', 'r')
+	json_file = open(model_name, 'r') #put name for model
+	loaded_model_json = json_file.read()
+	json_file.close()
+	loaded_model = model_from_json(loaded_model_json)
+	# load weights into new model
+	#loaded_model.load_weights("model.h5")
+	loaded_model.load_weights(model_weight) #put name for model weights
+	print("Loaded model from disk")
+
+	#load dataset
+	X_test1 = []
+	Y_test1 = []
+	#filename = 'vggNet_binary_1.hdf5'
+	filename = data #data file is loaded here
+	data = h5py.File(filename, "r")
+	print('Selected File: '+str(filename))
+	X_test1.append(np.array(data["test_img"]))
+	Y_test1.append(np.array(data["test_labels"]))
+	data.close()
+	X_test1 = np.concatenate(X_test1,axis=0)
+	Y_test1 = np.concatenate(Y_test1,axis=0)
+	print(X_test1.shape)
+	print(Y_test1.shape)
+	#Y_test1bin = to_categorical(np.argmax(Y_test1,-1)!=0) # binary
+	#print(Y_test1bin.shape)
+
+	from keras import optimizers
+	LEARNING_RATE = 1e-4
+
+	# evaluate loaded model on test data
+	loaded_model.compile(loss='binary_crossentropy', optimizer=optimizers.Adam(lr=LEARNING_RATE), metrics=['categorical_accuracy'])
+
+	preds = loaded_model.evaluate(X_test1, Y_test1)
+	print ("Loss = " + str(preds[0]))
+	print ("Test Accuracy = " + str(preds[1]))
 	
 def main():
 	#Open Data save in file
